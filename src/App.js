12608b8d1c71b08d55def56cf17d8db489824902
Pavel Che <alien.killer@bk.ru>
2021-09-18 13:26:02 +0300

    made layout main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+
+        <div className="d-flex align-center">
+          <img src="/img/logo.png" alt="logo" width={40} height={40} />
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img src="/img/cart.svg" alt="cart" width={18} height={18} />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img src="/img/user.svg" alt="cart" width={18} height={18} />
+          </li>
+        </ul>
+
       </header>
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
+
+        <div className="d-flex justify-around">
+          <div className="card">
+            <img src="/img/sneakers/1.jpg" alt="sneakers" width={133} height={112} />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column ">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="Plus" width={11} height={11} />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src="/img/sneakers/2.jpg" alt="sneakers" width={133} height={112} />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column ">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="Plus" width={11} height={11} />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src="/img/sneakers/3.jpg" alt="sneakers" width={133} height={112} />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column ">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="Plus" width={11} height={11} />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src="/img/sneakers/4.jpg" alt="sneakers" width={133} height={112} />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column ">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="Plus" width={11} height={11} />
+              </button>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
